c8a531f07189436dddd6a3eb10a03999d1a1ba68
Alfred J. Vaka <ajvaka20@gmail.com>
2025-09-05 21:10:57 -1000

    changes

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Show current year in footer
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Show last modified date in footer
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
